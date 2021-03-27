68056a4472b906aa6da7b10f7b37f2492d86d92e
lay295 <fizzohd@gmail.com>
2021-03-27 14:59:34 -0400

    Implement basic chat downloading

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 def main():
     print('Enter Video ID: ')
     videoId = input()
@@ -7,6 +10,29 @@ def main():
 # Download video as chat.json
 def download_chat(videoId):
     print('Downloading chat for ' + videoId)
+    comments = []
+    done = False
+    cursor = ''
+
+    while done is False:
+        requestUrl = 'https://api.twitch.tv/v5/videos/' + videoId + '/comments?' + ('content_offset_seconds=0' if comments.count == 0 else 'cursor=' + cursor)
+        response = requests.get(
+            requestUrl,
+            headers={
+                'Accept': 'application/vnd.twitchtv.v5+json; charset=UTF-8',
+                'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
+            }
+        )
+        json_response = response.json()
+        for comment in json_response['comments']:
+            comments.append(comment)
+        if "_next" in json_response:
+            cursor = json_response['_next']
+        else:
+            done = True
+    chatFile = open("chat.json", "w")
+    chatFile.write(json.dumps(comments))
+    
 
 # Download video as video.mp4
 def download_video(videoId):
